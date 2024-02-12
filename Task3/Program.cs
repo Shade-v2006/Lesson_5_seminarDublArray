b34bf2d3688e46e333e0d10cf63dc89a9f240794
Shade <Shade-v2006@yandex.ru>
2024-02-12 23:23:46 +0700

    Выполнена задача 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,15 +1,16 @@
 ﻿/*
-    Строка с наименьшей суммой элементов
-    Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-    Начальные условия:
+        Строка с наименьшей суммой элементов
+Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.
+Начальные условия:
 
-    int[,] numbers = new int[,] {
-        {1, 2, 3},
-        {1, 1, 0},
-        {7, 8, 2},
-        {9, 10, 11}
-    };
-    Выводится: 1
+int[,] numbers = new int[,] {
+    {1, 2, 3},
+    {1, 1, 0},
+    {7, 8, 2},
+    {9, 10, 11}
+};
+Выводится: 1
 */
 
 using System;
@@ -20,18 +21,35 @@ class UserInputToCompileForTest
     /// Вычисление сумм по строкам (на выходе массив с суммами строк)
     public static int[] SumRows(int[,] array)
     {
-      //Напишите свое решение здесь
-    
+        int[] sum = new int[array.GetLength(0)];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                sum[i] += array[i, j];
+            }
+        }
+        return sum;
     }
-    
+
     // Получение индекса минимального элемента в одномерном массиве
     public static int MinIndex(int[] array)
     {
-       //Напишите свое решение здесь
+        int minI = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[minI] > array[i])
+            {
+                minI = i;
+            }
+        }
+        return minI;
     }
     public static void PrintResult(int[,] numbers)
-    {   
-       //Напишите свое решение здесь
+    {
+        int[] sums = SumRows(numbers);
+        int minIndex = MinIndex(sums);
+        Console.Write(minIndex);
     }
 }
 
@@ -73,15 +91,14 @@ class Answer
         else
         {
             // Если аргументов на входе нет, используем примерный массив
-            
+
             numbers = new int[,] {
                 {1, 2, 3},
-                {1, 1, 0},
+                {1, 11, 0},
                 {7, 8, 2},
-                {9, 10, 11}
-    };      
+                {9, 1, 1}
+    };
         }
         UserInputToCompileForTest.PrintResult(numbers);
     }
 }
- 
